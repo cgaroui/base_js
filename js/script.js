22b49f4fea38129ad726b52002eae53197d56ca8
chaima garoui <chaimagaroui2@gmail.com>
2024-04-09 10:43:58 +0200

    exo1 ok (message alerte au click  avec props css)

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var carré = document.querySelector('.carré');   //selectionner le carré dans le DOM
+const cssObj = window.getComputedStyle(carré); //accéeder propriétés de style calculées de l'élément "carré"
+
+//ajout ecouteur d'evenement pour qu'il detecte le click et effectue une tache 
+carré.addEventListener('click',function(){
+    //ici la tache c'est afficher un message alerte avec les propriétés css
+    alert("Class : carré\n" +
+    "backgroundColor : " + cssObj.backgroundColor + "\n" +
+    "color : " + cssObj.color + "\n" +
+    "height : " + cssObj.height + "\n" +
+    "width : " + cssObj.width + "\n" +
+    "Display :" + cssObj.fontFamily);
+}
+)
+
+   

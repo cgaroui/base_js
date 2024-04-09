84a5e40c8cfbeb4396bec206c4ab0a4d993fbcdd
chaima garoui <chaimagaroui2@gmail.com>
2024-04-09 13:40:56 +0200

    exo2 début

---
+++ exo2/script2.js
@@ -0,0 +1,11 @@
+const carrés = document.querySelector("carrés");
+const carré5 = document.querySelector("carré5");
+const cssObj = window.getComputedStyle(carré5);
+
+carrés.addEventListener('click',function(){
+
+
+})
+
+
+

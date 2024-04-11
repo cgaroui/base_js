53b4baddb0f01c0c61f3b985f3ea1c39960ba480
chaima garoui <chaimagaroui2@gmail.com>
2024-04-11 13:52:51 +0200

    exo 2 le carré du bas  recupere bien la couleur du carré cliqué

--- exo2/script2.js
+++ exo2/script2.js
@@ -4,17 +4,16 @@ const carré = document.querySelectorAll(".carré"); //selectionner tout les ele
 const carré5 = document.querySelector(".carré5");// Sélectionnez l'élément carré5
 
 
-
+console.log(carré5)
 
 carré.forEach(carre=>{
-    const cssObj2 = window.getComputedStyle(carré5,null);
-    console.log(cssObj2.backgroundColor)
-   
+ 
     // console.log(carre);
     carre.addEventListener('click', function() {
         const cssObj = window.getComputedStyle(carre,null);
-        console.log(cssObj.backgroundColor)
-        // carré5.cssObj2.backgroundColor = cssObj.backgroundColor;       // search how to set background color js
+        //console.log(cssObj.backgroundColor)
+           //document.body.style.background = cssObj.backgroundColor;
+        carré5.style.backgroundColor = cssObj.backgroundColor;       // search how to set background color js
     
 })
 })

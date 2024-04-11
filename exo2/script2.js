5c00494238657b0ce8a58158dc659914c6ce67c2
chaima garoui <chaimagaroui2@gmail.com>
2024-04-11 15:05:55 +0200

    exo 2 ok , affichage du codde couleur + responsive

--- exo2/script2.js
+++ exo2/script2.js
@@ -7,16 +7,14 @@ const carré5 = document.querySelector(".carré5");// Sélectionnez l'élément 
 console.log(carré5)
 
 carré.forEach(carre=>{
- 
+
     // console.log(carre);
     carre.addEventListener('click', function() {
         const cssObj = window.getComputedStyle(carre,null);
         //console.log(cssObj.backgroundColor)
            //document.body.style.background = cssObj.backgroundColor;
-        carré5.style.backgroundColor = cssObj.backgroundColor;       // search how to set background color js
-    
+        carré5.style.backgroundColor = cssObj.backgroundColor;       //accede au style du carré5 et lui attribue la couleur du carré cliqué 
+
+        carré5.innerHTML= `<div class="msg"> ${cssObj.backgroundColor}</div>`
 })
 })
-
-
-

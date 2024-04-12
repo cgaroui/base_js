432284cfab01f1328746a9caf0394feaec6e9941
chaima garoui <chaimagaroui2@gmail.com>
2024-04-12 16:21:31 +0200

    exo4 affichage ok sans animation

--- exo3/stript.js
+++ exo3/stript.js
@@ -1,10 +1,27 @@
 
-var carre = document.querySelectorAll('.carre');
+var reseauS = document.querySelectorAll('.container');
 
 
    
-carre.forEach(carre => {
-     carre.addEventListener('click', function() {
-        carre.classList.toggle('rotation')      // fonction toggle sert a aller dans les 2 sens add et remove (la transition choisie )
+reseauS.forEach(container => {
+     container.addEventListener('click', function() {
+        const cssObj = window.getComputedStyle(container,null);
+        container.classList.toggle('transformation')      // fonction toggle sert a aller dans les 2 sens add et remove (la transition choisie )
+        document.body.style.backgroundColor = cssObj.backgroundColor;   
+        // alert.
+        
 })
+
 })
+
+
+const cssObj = window.getComputedStyle(carre,null);
+carré5.style.backgroundColor = cssObj.backgroundColor;   
+// var reseauS = document.querySelectorAll('.container');
+
+// reseauS.forEach(container => {
+//     container.addEventListener('click', function() {
+//         container.classList.toggle('rotation'); // Ajoute ou supprime la classe rotation à l'élément cliqué
+//         document.body.style.backgroundColor = window.getComputedStyle(container).getPropertyValue('background-color'); // Récupère la couleur d'arrière-plan de l'élément cliqué et l'applique au fond du document
+//     });
+// });

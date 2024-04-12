4df27ee02141735293fa6f04b125f4099f65a4f2
chaima garoui <chaimagaroui2@gmail.com>
2024-04-12 11:11:57 +0200

    exo 3 ok fonctionnel

--- exo3/stript.js
+++ exo3/stript.js
@@ -1,23 +1,10 @@
 
-var rotation = false ; 
-var carre = document.querySelectorAll(carre);
-
-carre.forEach(carre=>{
-    carre.addEventListener('click', function() {
+var carre = document.querySelectorAll('.carre');
 
 
-        carre.classList.toggle('rotated');
-        // degre = rotation ? 0 : 360;
-        
-        // carre.style.webkitTransform  = 'rotate ('+degre+')';
-        // carre.style.backgroundcolor = "#aa716c";
-        rotation = !rotation;
-       
-
-}
-)
+   
+carre.forEach(carre => {
+     carre.addEventListener('click', function() {
+        carre.classList.toggle('rotation')      // fonction toggle sert a aller dans les 2 sens add et remove (la transition choisie )
 })
-
-// document.querySelectorAll('carre').onclick = function() {
-//     document.querySelectorAll('carre').classList.toggle('rotated');
-// }
+})

c011494a4e63cfef974060b1f73598a4e9f6d0a0
chaima garoui <chaimagaroui2@gmail.com>
2024-04-15 17:00:50 +0200

    exo 5

--- exo5/script.js
+++ exo5/script.js
@@ -1,13 +1,12 @@
-var quotes = {
-    "Serge Karamazov ": "Barrez vous **** de mimes.",
-    "Georges Abitbol ": "le train de tes injures roule sur le rail de mon indifférence.",
-    "Hubert Bonisseur de la bath":"ça ne prenait pas beaucoup plus de temps.",
-    "Moulinier" : "L'habit ne fait pas le moine, mais il fait l'argent ...même non titularisé.",
-    "Orson Welles" : "c'est du vol et du plagiat. J'aime pas trop les voleurs et ..."
-};
 
-quotes.array.forEach(elment  => {
 
-    
-    
-});
+
+
+
+
+for (const property in quotes) {
+    console.log(`${property} : ${quotes[property]}`);
+    alert(`${property}+ "/n" + ${quotes[property]}`);
+  }
+  
+
